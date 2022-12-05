9e3e7b3ea95f61a1558fb899db85511bf2a7e37b
Daniel García <daniel.garcia@pjbc.gob.mx>
2022-12-05 12:04:50 -0800

    [WEB]: Automatic Index

---
+++ MM.CAAM/MM.CAAM.Admin.DTOs/HomeDto.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections.Generic;
+
+namespace MM.CAAM.Admin.DTOs
+{
+    public class HomeDto
+    {
+        public string Marca { get; set; }
+        public string Descripcion { get; set; }
+        public string Title { get; set; }
+        public string Eslogan { get; set; }
+
+        public List<InfoHomeDto> ListInfoHome { get; set; } = new List<InfoHomeDto>();
+
+        public string UrlFacebook { get; set; }
+        public string TextFacebook { get; set; }
+    }
+}
--- MM.CAAM/MM.CAAM.Web/Controllers/HomeController.cs
+++ MM.CAAM/MM.CAAM.Web/Controllers/HomeController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using MM.CAAM.Admin.DTOs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MM.CAAM.Web.Controllers
@@ -10,7 +10,65 @@ namespace MM.CAAM.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var homeDto = new HomeDto
+            {
+                Title = "Caam: Atención Profesional en Psicología",
+                Marca = "CAAM",
+                Descripcion = "Psicología Profesional CAAM.",
+                Eslogan = "Por una buena educación.",
+                UrlFacebook = "https://www.facebook.com/psicologia.caam",
+                TextFacebook = "Learn more"
+            };
+
+            homeDto.ListInfoHome = new List<InfoHomeDto>()
+            {
+                new InfoHomeDto()
+                {
+                    Titulo = "Infantil",
+                    Contenidos = new List<string>()
+                    {
+                        "El psicólogo infantil se encarga del proceso de evaluación y diagnóstico de niños y adolescentes. Es el profesional capacitado para diagnosticar los trastornos del desarrollo de índole psicológica que pueden aparecer en este periodo evolutivo, desde el autismo hasta las dificultades de aprendizaje."
+                    }
+                },
+                new InfoHomeDto()
+                {
+                    Titulo = "Familiar",
+                    Contenidos = new List<string>()
+                    {
+                        "¿Cómo se aplica la psicologia familiar?"
+                        ,"Resultado de imagen para psicologia familiar La psicología familiar analiza y trata de resolver esos conflictos que se plantean en el grupo familiar. En la psicología familiar se estudian las familias como sistemas y se analizan comportamientos, patrones de comunicación y respuestas emocionales entre los miembros del grupo familiar."
+                        ,"¿Por qué es importante la psicología en la familia?"
+                        ,"Función de apoyo y protección psicosocial: Ejerce un efecto protector y estabilizador frente a los trastornos mentales, la familia facilita la adaptación de sus miembros a las nuevas circunstancias en consonancia con el entorno social. Según Nathan W."
+                    }
+                },
+                new InfoHomeDto()
+                {
+                    Titulo = "Pareja",
+                    Contenidos = new List<string>()
+                    {
+                        "¿Cómo saber si tengo que ir a terapia de pareja?",
+                        "Los principales motivos por los que se recurre a una Terapia de Pareja son: ",
+                        "1. Dificultades en la sexualidad, disfunciones sexuales o desacuerdos en la frecuencia de las relaciones.",
+                        "2. Monotonía y aburrimiento en la relación de pareja.",
+                        "3. Problemas en la comunicación.",
+                        "4. Infidelidades.",
+                        "5. Celos y desconfianza en la pareja."
+                    }
+                },
+                new InfoHomeDto()
+                {
+                    Titulo = "Individual",
+                    Contenidos = new List<string>()
+                    {
+                        "La terapia individual se suele emplear para trabajar los problemas de ansiedad, la depresión, la inseguridad personal, la baja autoestima, la toma de decisiones, el autocontrol de las emociones, la sensación de estancamiento en la vida (en el área sentimental, la vida laboral, las relaciones con otras personas del entorno, etc.), y los problemas sexuales, en aquellos casos en los que no se tiene una pareja o ésta no quiere participar en la terapia."
+                    }
+                }
+            };
+            
+
+            
+
+            return View(homeDto);
         }
 
         public ActionResult About()
