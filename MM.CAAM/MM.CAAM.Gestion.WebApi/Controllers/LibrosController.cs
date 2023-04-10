8b068992317b048944ef71be2e26cb975aafac59
Daniel.Garcia <sanck.one@gmail.com>
2023-04-10 14:19:55 -0700

    66: Borrando recursos finalizado.

--- MM.CAAM/MM.CAAM.Gestion.WebApi/Controllers/LibrosController.cs
+++ MM.CAAM/MM.CAAM.Gestion.WebApi/Controllers/LibrosController.cs
@@ -30,6 +30,11 @@ namespace MM.CAAM.Gestion.WebApi.Controllers
                 .ThenInclude(autorLibroDB => autorLibroDB.Autor)
                 .FirstOrDefaultAsync(x => x.Id == id);
 
+            if(libro == null)
+            {
+                return NotFound();
+            }
+
             libro.AutoresLibros = libro.AutoresLibros.OrderBy(x => x.Orden).ToList();
 
             return mapper.Map<LibroDTOConAutores>(libro);
@@ -132,6 +137,21 @@ namespace MM.CAAM.Gestion.WebApi.Controllers
 
             await context.SaveChangesAsync();
             return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe = await context.Libros.AnyAsync(x => x.Id == id);
+
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Libro() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
         }
     }
 }
