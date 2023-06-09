c731682dd5e3934fd22da8faf3583786f9429c0a
Daniel García <daniel.garcia@pjbc.gob.mx>
2023-06-09 01:21:58 -0700

    [WebApi]: avance con orden de catalogos

---
+++ MM.CAAM/MM.CAAM.Gestion.WebApi/Migrations/20230609080422_Se_agrega_orden_en_catalogos.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MM.CAAM.Gestion.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Seagregaordenencatalogos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Orden",
+                table: "UnidadesMedida",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Orden",
+                table: "TiposGrupo",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Orden",
+                table: "SeccionesSupermercado",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Orden",
+                table: "Proveedores",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Orden",
+                table: "GrupoAlimenticios",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "UnidadesMedida");
+
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "TiposGrupo");
+
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "SeccionesSupermercado");
+
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "Proveedores");
+
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "GrupoAlimenticios");
+        }
+    }
+}
