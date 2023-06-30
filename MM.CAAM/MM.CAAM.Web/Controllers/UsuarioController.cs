28de31f46cd4e818478b54c3dd7433f6fa0677dc
Daniel García <daniel.garcia@pjbc.gob.mx>
2023-06-29 18:29:05 -0700

    [MVC]: Avance usuarios

---
+++ MM.CAAM/MM.CAAM.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace MM.CAAM.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AgregarUsuario()
+        {
+            return View();
+        }
+    }
+}
