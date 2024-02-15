6fdee6da25df2a0cecf35666a86b5bb94faf3347
Daniel García <daniel.garcia@pjbc.gob.mx>
2024-02-14 22:32:44 -0800

    avance

--- MM.CAAM/MM.CAAM.Gestion.WebApi/Controllers/UsuariosController.cs
+++ MM.CAAM/MM.CAAM.Gestion.WebApi/Controllers/UsuariosController.cs
@@ -48,172 +48,159 @@ namespace MM.CAAM.Gestion.Models.Controllers
         [HttpGet]
         public async Task<ActionResult<List<UsuarioDTO>>> Get()
         {
-            //try
-            //{ 
-            //    var usuarios = await context.Usuarios
-            //        .Where(x => x.Activo == true)
-            //        .OrderByDescending(u => u.Id).ToListAsync();
+            try
+            {
+                var usuarios = await context.Usuarios
+                    .Where(x => x.Activo == true)
+                    .OrderByDescending(u => u.Id).ToListAsync();
 
-            //    var data = mapper.Map<List<UsuarioDTO>>(usuarios);
+                var data = mapper.Map<List<UsuarioDTO>>(usuarios);
 
-            //    return Ok(new Result { Code = StatusCodes.Status200OK, Data = data });
-            //}
-            //catch (ValidationException ex)
-            //{
-            //    var error = new ExceptionMessage(ex);
-            //    return StatusCode(StatusCodes.Status400BadRequest, new Result { Code = StatusCodes.Status400BadRequest, Message = error.MessageException });
-            //}
-            //catch (Exception ex)
-            //{
-            //    var error = new ExceptionMessage(ex);
-            //    return StatusCode(StatusCodes.Status500InternalServerError, new Result { Code = StatusCodes.Status500InternalServerError, Message = error.MessageException });
-            //}
-
-            return null;
+                return Ok(new Result { Code = StatusCodes.Status200OK, Data = data });
+            }
+            catch (ValidationException ex)
+            {
+                var error = new ExceptionMessage(ex);
+                return StatusCode(StatusCodes.Status400BadRequest, new Result { Code = StatusCodes.Status400BadRequest, Message = error.MessageException });
+            }
+            catch (Exception ex)
+            {
+                var error = new ExceptionMessage(ex);
+                return StatusCode(StatusCodes.Status500InternalServerError, new Result { Code = StatusCodes.Status500InternalServerError, Message = error.MessageException });
+            } 
         }
 
         [HttpGet("{id:int}", Name = "ObtenerUsuario")]
         public async Task<ActionResult<UsuarioDTO>> Get(int id)
         {
-            //try
-            //{
+            try
+            {
 
-            //    var usuario = await context.Usuarios
-            //        .Where(x => x.Activo == true)
-            //        .Include(usuarioDB => usuarioDB.UsuariosNegocios)
-            //        .ThenInclude(usuariosNegociosDB => usuariosNegociosDB.Negocio)
-            //        .FirstOrDefaultAsync(usuarioBD => usuarioBD.Id == id); //usuarioBD.Negocios || Consultas
+                var usuario = await context.Usuarios
+                    .Where(x => x.Activo == true)
+                    .Include(usuarioDB => usuarioDB.TipoUsuario) 
+                    .FirstOrDefaultAsync(usuarioBD => usuarioBD.Id == id); //usuarioBD.Negocios || Consultas
 
-            //    if (usuario == null)
-            //    {
-            //        throw new ArgumentNullException(nameof(usuario));
-            //    }
+                if (usuario == null)
+                {
+                    throw new ArgumentNullException(nameof(usuario));
+                }
 
-            //    var data = mapper.Map<UsuarioDTO>(usuario);
+                var data = mapper.Map<UsuarioDTO>(usuario);
 
-            //    return Ok(new Result { Code = StatusCodes.Status200OK, Data = data });
-            //}
-            //catch (ValidationException ex)
-            //{
-            //    var error = new ExceptionMessage(ex);
-            //    return StatusCode(StatusCodes.Status400BadRequest, new Result { Code = StatusCodes.Status400BadRequest, Message = error.MessageException });
-            //}
-            //catch (Exception ex)
-            //{
-            //    var error = new ExceptionMessage(ex);
-            //    return StatusCode(StatusCodes.Status500InternalServerError, new Result { Code = StatusCodes.Status500InternalServerError, Message = error.MessageException });
-            //}
-
-            return null;
+                return Ok(new Result { Code = StatusCodes.Status200OK, Data = data });
+            }
+            catch (ValidationException ex)
+            {
+                var error = new ExceptionMessage(ex);
+                return StatusCode(StatusCodes.Status400BadRequest, new Result { Code = StatusCodes.Status400BadRequest, Message = error.MessageException });
+            }
+            catch (Exception ex)
+            {
+                var error = new ExceptionMessage(ex);
+                return StatusCode(StatusCodes.Status500InternalServerError, new Result { Code = StatusCodes.Status500InternalServerError, Message = error.MessageException });
+            }
         }
 
         [HttpGet("{nombre}")]
         public async Task<ActionResult<List<UsuarioDTO>>> Get([FromRoute] string nombre)
         {
-            //var usuarios = await context.Usuarios
-            //    .Where(UsuarioBd => UsuarioBd.Nombre.Contains(nombre) && UsuarioBd.Activo == true)
-            //    .ToListAsync();
+            var usuarios = await context.Usuarios
+                .Where(UsuarioBd => UsuarioBd.Nombre.Contains(nombre) && UsuarioBd.Activo == true)
+                .ToListAsync();
 
-            //return mapper.Map<List<UsuarioDTO>>(usuarios);
-
-            return null;
+            return mapper.Map<List<UsuarioDTO>>(usuarios);
         }
 
         [HttpPost]
         public async Task<ActionResult> Post([FromBody] UsuarioCreacionDTO usuarioCreacionDTO)
         {
-            //try
-            //{
-            //    if (!string.IsNullOrEmpty(usuarioCreacionDTO.NombrePerfil) || !string.IsNullOrEmpty(usuarioCreacionDTO.Correo))
-            //    {
-            //        var userRepetido = await context.Usuarios.Where(x => (!string.IsNullOrEmpty(usuarioCreacionDTO.Correo) && x.Correo.Equals(usuarioCreacionDTO.Correo))
-            //                                                                         || (!string.IsNullOrEmpty(usuarioCreacionDTO.NombrePerfil) && x.NombrePerfil.Equals(usuarioCreacionDTO.NombrePerfil))).FirstOrDefaultAsync();
+            try
+            {
+                if (!string.IsNullOrEmpty(usuarioCreacionDTO.NombrePerfil) || !string.IsNullOrEmpty(usuarioCreacionDTO.Correo))
+                {
+                    var userRepetido = await context.Usuarios.Where(x => (!string.IsNullOrEmpty(usuarioCreacionDTO.Correo) && x.Correo.Equals(usuarioCreacionDTO.Correo))
+                                                                                     || (!string.IsNullOrEmpty(usuarioCreacionDTO.NombrePerfil) && x.NombrePerfil.Equals(usuarioCreacionDTO.NombrePerfil))).FirstOrDefaultAsync();
 
-            //        if (userRepetido != null && (!string.IsNullOrEmpty(usuarioCreacionDTO.NombrePerfil) || !string.IsNullOrEmpty(usuarioCreacionDTO.Correo)))
-            //        {
-            //            var valorRepetido = !string.IsNullOrEmpty(userRepetido.Correo) && userRepetido.Correo.ToUpper().Equals(usuarioCreacionDTO.Correo.ToUpper()) ? $"Ya existe el correo: {usuarioCreacionDTO.Correo}" :
-            //                                                                                        $"Ya existe el usuario: {usuarioCreacionDTO.NombrePerfil}";
+                    if (userRepetido != null && (!string.IsNullOrEmpty(usuarioCreacionDTO.NombrePerfil) || !string.IsNullOrEmpty(usuarioCreacionDTO.Correo)))
+                    {
+                        var valorRepetido = !string.IsNullOrEmpty(userRepetido.Correo) && userRepetido.Correo.ToUpper().Equals(usuarioCreacionDTO.Correo.ToUpper()) ? $"Ya existe el correo: {usuarioCreacionDTO.Correo}" :
+                                                                                                    $"Ya existe el usuario: {usuarioCreacionDTO.NombrePerfil}";
 
-            //            throw new ArgumentException(valorRepetido);
-            //        }
-            //    }
+                        throw new ArgumentException(valorRepetido);
+                    }
+                }
 
-            //    #region SET VALORES
-            //    if (!string.IsNullOrEmpty(usuarioCreacionDTO.Password))
-            //    {
-            //        //usuarioCreacionDTO.Password = Encryptor(usuarioCreacionDTO.Password);
-            //        usuarioCreacionDTO.Password = usuarioCreacionDTO.Password;
-            //    }
-            //    usuarioCreacionDTO.FechaCreacion = Com.GetUtcNowByZone();
+                #region SET VALORES
+                if (!string.IsNullOrEmpty(usuarioCreacionDTO.Password))
+                {
+                    //usuarioCreacionDTO.Password = Encryptor(usuarioCreacionDTO.Password);
+                    usuarioCreacionDTO.Password = usuarioCreacionDTO.Password;
+                }
+                usuarioCreacionDTO.FechaCreacion = Com.GetUtcNowByZone();
 
-            //    #endregion
+                #endregion
 
-            //    #region NOMBRE MAYUSCULAS
-            //    usuarioCreacionDTO.Nombre = usuarioCreacionDTO.Nombre.ToUpper();
-            //    usuarioCreacionDTO.ApellidoPaterno = usuarioCreacionDTO.ApellidoPaterno.ToUpper();
-            //    usuarioCreacionDTO.ApellidoMaterno = usuarioCreacionDTO.ApellidoMaterno.ToUpper();
-            //    #endregion
+                #region NOMBRE MAYUSCULAS
+                usuarioCreacionDTO.Nombre = usuarioCreacionDTO.Nombre.ToUpper();
+                usuarioCreacionDTO.ApellidoPaterno = usuarioCreacionDTO.ApellidoPaterno.ToUpper();
+                usuarioCreacionDTO.ApellidoMaterno = usuarioCreacionDTO.ApellidoMaterno.ToUpper();
+                #endregion
 
-            //    var usuario = mapper.Map<Usuario>(usuarioCreacionDTO);
+                var usuario = mapper.Map<Usuario>(usuarioCreacionDTO);
 
-            //    context.Add(usuario);
-            //    await context.SaveChangesAsync();
+                context.Add(usuario);
+                await context.SaveChangesAsync();
 
-            //    var usuarioDTO = mapper.Map<UsuarioDTO>(usuario);
+                var usuarioDTO = mapper.Map<UsuarioDTO>(usuario);
 
-            //    return Ok(new Result { Code = StatusCodes.Status200OK });
-            //}
-            //catch (ValidationException ex)
-            //{
-            //    var error = new ExceptionMessage(ex);
-            //    return StatusCode(StatusCodes.Status400BadRequest, new Result { Code = StatusCodes.Status400BadRequest, Message = error.MessageException });
-            //}
-            //catch (Exception ex)
-            //{
-            //    var error = new ExceptionMessage(ex);
-            //    return StatusCode(StatusCodes.Status500InternalServerError, new Result { Code = StatusCodes.Status500InternalServerError, Message = error.MessageException });
-            //}
-
-            return null;
+                return Ok(new Result { Code = StatusCodes.Status200OK });
+            }
+            catch (ValidationException ex)
+            {
+                var error = new ExceptionMessage(ex);
+                return StatusCode(StatusCodes.Status400BadRequest, new Result { Code = StatusCodes.Status400BadRequest, Message = error.MessageException });
+            }
+            catch (Exception ex)
+            {
+                var error = new ExceptionMessage(ex);
+                return StatusCode(StatusCodes.Status500InternalServerError, new Result { Code = StatusCodes.Status500InternalServerError, Message = error.MessageException });
+            } 
         }
 
         [HttpPut("{id:int}")]
         public async Task<ActionResult> Put(Usuario usuario, int id)
         {
-            //if (usuario.Id != id)
-            //{
-            //    return BadRequest("El id del usuario no coincide con el id de la URL");
-            //}
+            if (usuario.Id != id)
+            {
+                return BadRequest("El id del usuario no coincide con el id de la URL");
+            }
 
-            //var existe = await context.Usuarios.AnyAsync(us => us.Id == id);
+            var existe = await context.Usuarios.AnyAsync(us => us.Id == id);
 
-            //if (!existe)
-            //{
-            //    return NotFound();
-            //}
+            if (!existe)
+            {
+                return NotFound();
+            }
 
-            //context.Update(usuario);
-            //await context.SaveChangesAsync();
-            //return Ok();
-
-            return null;
+            context.Update(usuario);
+            await context.SaveChangesAsync();
+            return Ok();
         }
 
         [HttpDelete("{id:int}")]
         public async Task<ActionResult> Delete(int id)
         {
-            //var existe = await context.Usuarios.AnyAsync(us => us.Id == id);
+            var existe = await context.Usuarios.AnyAsync(us => us.Id == id);
 
-            //if (!existe)
-            //{
-            //    return NotFound();
-            //}
+            if (!existe)
+            {
+                return NotFound();
+            }
 
-            //context.Remove(new Usuario { Id = id });
-            //await context.SaveChangesAsync();
-            //return Ok();
-
-            return null;
+            context.Remove(new Usuario { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
         }
 
         [HttpGet("desencriptar")]
@@ -237,50 +224,48 @@ namespace MM.CAAM.Gestion.Models.Controllers
         [HttpPost("login")]
         public async Task<ActionResult> Login([FromBody] UserLoginRequest userLoginRequest)
         {
-            //try
-            //{
-            //    #region (1) UsuarioService.Login
-            //    if (userLoginRequest == null) throw new ArgumentNullException(nameof(userLoginRequest));
-            //    if (string.IsNullOrWhiteSpace(userLoginRequest.Email)) throw new ArgumentNullException(nameof(userLoginRequest.Email));
-            //    if (string.IsNullOrWhiteSpace(userLoginRequest.Password)) throw new ArgumentNullException(nameof(userLoginRequest.Password));
+            try
+            {
+                #region (1) UsuarioService.Login
+                if (userLoginRequest == null) throw new ArgumentNullException(nameof(userLoginRequest));
+                if (string.IsNullOrWhiteSpace(userLoginRequest.Email)) throw new ArgumentNullException(nameof(userLoginRequest.Email));
+                if (string.IsNullOrWhiteSpace(userLoginRequest.Password)) throw new ArgumentNullException(nameof(userLoginRequest.Password));
 
-            //    var usuario = await context.Usuarios
-            //                        .Where(x => x.Activo == true)
-            //                        .FirstOrDefaultAsync(usuarioBD => usuarioBD.Correo.ToUpper() == userLoginRequest.Email.ToUpper() );
+                var usuario = await context.Usuarios
+                                    .Where(x => x.Activo == true)
+                                    .FirstOrDefaultAsync(usuarioBD => usuarioBD.Correo.ToUpper() == userLoginRequest.Email.ToUpper());
 
-            //    if (usuario == null)
-            //    {
-            //        throw new ValidationException("El usuario no existe en nuestra app");
-            //    }
+                if (usuario == null)
+                {
+                    throw new ValidationException("El usuario no existe en nuestra app");
+                }
 
-            //    if (string.IsNullOrWhiteSpace(usuario.Password))
-            //    {
-            //        throw new ValidationException("Configure su contraseña");
-            //    }
+                if (string.IsNullOrWhiteSpace(usuario.Password))
+                {
+                    throw new ValidationException("Configure su contraseña");
+                }
 
-            //    //var contrasenaDesencriptada = Decryptor(usuario.Password);
-            //    var contrasenaDesencriptada = usuario.Password;
-            //    if (!contrasenaDesencriptada.Equals(userLoginRequest.Password))
-            //    {
-            //        throw new ValidationException("Error de contraseña");
-            //    }
-            //    #endregion  
+                //var contrasenaDesencriptada = Decryptor(usuario.Password);
+                var contrasenaDesencriptada = usuario.Password;
+                if (!contrasenaDesencriptada.Equals(userLoginRequest.Password))
+                {
+                    throw new ValidationException("Error de contraseña");
+                }
+                #endregion  
 
-            //    var data = await ConstruirToken(usuario);
-            //    return Ok(new Result { Code = StatusCodes.Status200OK, Data = data });
-            //}
-            //catch (ValidationException ex)
-            //{
-            //    var error = new ExceptionMessage(ex);
-            //    return StatusCode(StatusCodes.Status400BadRequest, new Result { Code = StatusCodes.Status400BadRequest, Message = error.MessageException });
-            //}
-            //catch (Exception ex)
-            //{
-            //    var error = new ExceptionMessage(ex);
-            //    return StatusCode(StatusCodes.Status500InternalServerError, new Result { Code = StatusCodes.Status500InternalServerError, Message = error.MessageException });
-            //}
-
-            return null;
+                var data = await ConstruirToken(usuario);
+                return Ok(new Result { Code = StatusCodes.Status200OK, Data = data });
+            }
+            catch (ValidationException ex)
+            {
+                var error = new ExceptionMessage(ex);
+                return StatusCode(StatusCodes.Status400BadRequest, new Result { Code = StatusCodes.Status400BadRequest, Message = error.MessageException });
+            }
+            catch (Exception ex)
+            {
+                var error = new ExceptionMessage(ex);
+                return StatusCode(StatusCodes.Status500InternalServerError, new Result { Code = StatusCodes.Status500InternalServerError, Message = error.MessageException });
+            }
         }
 
         private async Task<AuthResponse> ConstruirToken(Usuario usuario)
@@ -310,10 +295,7 @@ namespace MM.CAAM.Gestion.Models.Controllers
             var newJwtToken = tokenString;
 
             #endregion
-
-
-
-
+             
             var authResponse = new AuthResponse()
             {
                 UsuarioId = usuario.Id,                  //TODO:
